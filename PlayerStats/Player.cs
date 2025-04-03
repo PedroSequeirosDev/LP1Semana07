908ef7304ae83a31f1f22d3bd2202890b930e4fe
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-04-03 22:26:20 +0100

    add PlayerStats Project with initial variables

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerStats
+{
+    public class Player
+    {
+        private float highScore;
+
+        private int playedGames;
+
+        private int wonGames;
+
+        private float ValueofRun;
+
+        private readonly string name;
+
+
+    }
+}
