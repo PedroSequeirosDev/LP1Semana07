38a97083c231610aef3edf283d7bdcdf9c18f654
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-04-04 16:59:42 +0100

    add hero.cs class and instance variables

---
+++ MyRoguelike/Hero.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRoguelike
+{
+    public class Hero
+    {
+        private int xp;
+        private float health;
+    }
+}
