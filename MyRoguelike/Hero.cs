83a8510e40294e55acc31a0867747123d783014f
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-04-04 17:10:50 +0100

    add health property

--- MyRoguelike/Hero.cs
+++ MyRoguelike/Hero.cs
@@ -31,8 +31,27 @@ namespace MyRoguelike
         {
             get => 1 + XP / 1000;
         }
+
+        public float Health
+        {
+            get => health;
+            set
+            {
+                if (value > 0 && value < Maxhealth)
+                {
+                    health = value;
+                }
+                else if (health <= 0)
+                {
+                    health = 0;
+                }
+            }
+        }
+
+
+
     }
-
-
-
 }
+
+
+
